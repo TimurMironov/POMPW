ba67558a6dfefb52151308f1b248cd092bd88385
unknown <mironov.timur.99@mail.ru>
2026-01-08 20:02:30 +0300

    fix test

--- pages/base_page.py
+++ pages/base_page.py
@@ -10,12 +10,14 @@ class BasePage:
     def open_page(self):
         with allure.step(f"Открыть страницу {self.PAGE_URL}"):
             self.page.context.clear_cookies()
-            self.page.goto(self.PAGE_URL)
+            self.page.goto(self.PAGE_URL, timeout=60000)
             self.page.wait_for_load_state("networkidle")
 
     def is_opened(self):
         with allure.step(f"Проверить, что открыта страница {self.PAGE_URL}"):
+            self.page.wait_for_load_state("networkidle")
             expect(self.page).to_have_url(self.PAGE_URL)
+
 
     def make_screenshot(self):
         allure.attach(
